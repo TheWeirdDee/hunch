ff038f09d3506b76eec3ca806bded16a75353936
Divine <your-email@divinenation1.com>
2024-12-06 22:21:16 +1400

    Commit

--- src/Components/HunchIs.jsx
+++ src/Components/HunchIs.jsx
@@ -1,4 +1,4 @@
-/* eslint-disable no-unused-vars */
+ /* eslint-disable no-unused-vars */
 import React from "react";
 import Hunchlunch from "../assets/Hunchlunch.png";
 import Union from "../assets/Union.png";
